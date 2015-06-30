4b4175f93d8f16d17d07f0b5e1ceefa31d75d050
Mats Jalas <mjalas@kovasolutions.com>
2015-06-30 11:35:29 +0300

    Changed file name to provision.sh

--- provision.sh
+++ provision.sh
@@ -3,10 +3,17 @@ echo "Updating repos and upgrading system..."
 apt-get update > /dev/null 2>&1
 apt-get -y upgrade > /dev/null 2>&1
 echo "Done."
+
 echo "Setup environment:"
 echo "Installing texlive-latex-extra..."
 apt-get -y install texlive-latex-extra > /dev/null 2>&1
 echo "Installing latexmk..."
 apt-get -y install latexmk > /dev/null 2>&1
+
+echo "Installing languages (finnish, swedish)..."
+apt-get -y install texlive-lang-finnish > /dev/null 2>&1
+apt-get -y install texlive-lang-swedish > /dev/null 2>&1
+
+
 echo "Environment setup done."
 echo "Happy latexing!"
